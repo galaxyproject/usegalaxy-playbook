c3672f070c2f0ae185e8871ac54c1298c46525de
Nate Coraor <nate@bx.psu.edu>
2014-09-08 13:08:40 -0500

    Add usegalaxy.org (Main) files, templates, and associated vars/inventory.

---
+++ files/galaxy/usegalaxy.org/dynamic_rules/reserved.py
@@ -0,0 +1,17 @@
+##
+## This file is maintained by Ansible - CHANGES WILL BE OVERWRITTEN
+##
+
+USERS = (
+    'outreach@galaxyproject.org',
+)
+
+def reserved_single( user_email ):
+    if user_email in USERS:
+        return 'roundup_single_reserved'
+    return 'roundup_single'
+
+def reserved_multi( user_email ):
+    if user_email in USERS:
+        return 'roundup_multi_reserved'
+    return 'roundup_multi_static_walltime'
