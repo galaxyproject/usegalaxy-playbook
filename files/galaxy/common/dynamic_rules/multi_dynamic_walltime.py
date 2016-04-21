3eec599a7c17e57f3d1d85ce5d740fb98e162af0
Nate Coraor <nate@bx.psu.edu>
2016-04-21 16:43:41 -0500

    Test and Main updates, Jetstream config changes and fixes for Test, enable Jetstream destinations for Main

--- files/galaxy/common/dynamic_rules/multi_dynamic_walltime.py
+++ files/galaxy/common/dynamic_rules/multi_dynamic_walltime.py
@@ -2,8 +2,9 @@
 ## This file is maintained by Ansible - CHANGES WILL BE OVERWRITTEN
 ##
 
-import logging
 import datetime
+import logging
+import subprocess
 from galaxy.jobs.mapper import JobMappingException
 
 log = logging.getLogger(__name__)
@@ -22,6 +23,7 @@ log = logging.getLogger(__name__)
 # bowtie_color_wrapper  same as bwa_wrapper
 # bwa_color_wrapper     same as bowtie_wrapper
 
+JETSTREAM_TOOLS = ( 'bowtie2', 'bwa', 'bwa_mem', 'tophat2', 'cufflinks' )
 PUNT_TOOLS = ( 'bwa_wrapper', 'bowtie2', 'bowtie_wrapper', 'tophat', 'tophat2', 'bwa', 'bwa_mem' )
 GENOME_SOURCE_PARAMS = ( 'genomeSource.refGenomeSource', 'reference_genome.source', 'refGenomeSource.genomeSource', 'reference_source.reference_source_selector' )
 GENOME_SOURCE_VALUES = ( 'indexed', 'cached' )
@@ -32,7 +34,7 @@ LOCAL_DEVELOPMENT_DESTINATION = 'slurm_multi_development'
 STAMPEDE_DESTINATION = 'pulsar_stampede_normal'
 STAMPEDE_DEVELOPMENT_DESTINATION = 'pulsar_stampede_development'
 STAMPEDE_DESTINATIONS = (STAMPEDE_DESTINATION, STAMPEDE_DEVELOPMENT_DESTINATION)
-JETSTREAM_DESTINATIONS = ('jetstream_large',)
+JETSTREAM_DESTINATIONS = ('jetstream_multi',)
 VALID_DESTINATIONS = (LOCAL_DESTINATION, LOCAL_DEVELOPMENT_DESTINATION) + STAMPEDE_DESTINATIONS + JETSTREAM_DESTINATIONS # WHY ARE WE SHOUTING
 RESOURCES = {'tacc_compute_resource':VALID_DESTINATIONS, 'stampede_compute_resource':STAMPEDE_DESTINATIONS}
 FAILURE_MESSAGE = 'This tool could not be run because of a misconfiguration in the Galaxy job running system, please report this error'
@@ -50,6 +52,24 @@ TEAM_USERS = (
     'anton@bx.psu.edu'
 )
 TEAM_DESTINATION = 'reserved_dynamic'
+
+JETSTREAM_DESTINATION_MAPS = {
+    LOCAL_DESTINATION: {
+        'clusters': ['jetstream-iu', 'jetstream-tacc', 'roundup'],
+        'cluster_prefixes': ['jetstream-iu-large', 'jetstream-tacc-large', 'roundup'],
+        'destination_prefixes': ['jetstream_iu', 'jetstream_tacc', 'slurm'],
+        'partition': 'multi',
+    },
+    'jetstream_multi': {
+        'clusters': ['jetstream-iu', 'jetstream-tacc'],
+        'cluster_prefixes': ['jetstream-iu-large', 'jetstream-tacc-large'],
+        'destination_prefixes': ['jetstream_iu', 'jetstream_tacc'],
+        'partition': 'multi',
+    },
+}
+
+# Just needs to be a shell script, does not matter what it is
+TEST_SCRIPT = '/usr/bin/ldd'
 
 # collected with:
 # python runtime_stats.py -c ./galaxy.ini -m 120 -M $((34 * 60 * 60)) --source=metrics --like
@@ -71,9 +91,10 @@ RUNTIMES = {
     #'stringtie': {'runtime': 165.47, 'stddev': 286.19, 'devs': 2},
 }
 
-def __rule( app, tool, job, user_email, resource ):
+def __rule(app, tool, job, user_email, resource):
     destination = None
-    destination_id = LOCAL_DESTINATION
+    destination_id = None
+    default_destination_id = RESOURCES[resource][0]
     tool_id = tool.id
 
     if '/' in tool.id:
@@ -104,7 +125,7 @@ def __rule( app, tool, job, user_email, resource ):
                 log.warning('(%s) Destination/walltime dynamic plugin got an invalid value for selector: %s', job.id, param_dict['__job_resource']['__job_resource__select'])
                 raise JobMappingException( FAILURE_MESSAGE )
         elif param_dict['__job_resource']['__job_resource__select'] == 'no':
-            destination_id = RESOURCES[resource][0]
+            # job will be sent to the default
             if user_email.lower() in NORM_RESERVED_USERS:
                 log.info("(%s) Destination/walltime dynamic plugin returning default reserved destination for '%s'", job.id, user_email)
                 return RESERVED_DESTINATION
@@ -142,7 +163,64 @@ def __rule( app, tool, job, user_email, resource ):
             if destination_id == STAMPEDE_DEVELOPMENT_DESTINATION:
                 destination_id = LOCAL_DEVELOPMENT_DESTINATION
             else:
-                destination_id = LOCAL_DESTINATION
+                destination_id = default_destination_id
+
+    # Some tools do not react well to Jetstream
+    if destination_id is None and tool_id not in JETSTREAM_TOOLS:
+        log.info('(%s) Default destination requested and tool is not in Jetstream-approved list, job will be sent to local cluster', job.id)
+        destination_id = default_destination_id
+
+    # Only allow jetstream if a cached reference is not selected
+    #if destination_id in JETSTREAM_DESTINATIONS + (None,) and tool_id in PUNT_TOOLS:
+    #    for p in GENOME_SOURCE_PARAMS:
+    #        subpd = param_dict.copy()
+    #        # walk the param dict
+    #        try:
+    #            for i in p.split('.'):
+    #                subpd = subpd[i]
+    #            assert subpd not in GENOME_SOURCE_VALUES
+    #            log.info('(%s) Destination/walltime dynamic plugin detected history reference selected, job will be allowed on Jetstream', job.id)
+    #            break
+    #        except:
+    #            pass
+    #    else:
+    #        log.info('(%s) User requested Jetstream or default but destination/walltime dynamic plugin did not detect selection of a history reference, job will be sent to local cluster instead', job.id)
+    #        destination_id = default_destination_id
+
+    # Need to explicitly pick a destination because of staging. Otherwise we
+    # could just submit with --clusters=a,b,c and let slurm sort it out
+    if destination_id in JETSTREAM_DESTINATIONS + (None,) and default_destination_id == LOCAL_DESTINATION:
+        test_destination_id = destination_id or default_destination_id
+        clusters = ','.join(JETSTREAM_DESTINATION_MAPS[test_destination_id]['clusters'])
+        native_specification = app.job_config.get_destination(test_destination_id).params.get('nativeSpecification', '')
+        sbatch_test_cmd = ['sbatch', '--test-only', '--clusters=%s' % clusters] + native_specification.split() + [TEST_SCRIPT]
+        log.debug('Testing job submission to determine suitable cluster: %s', ' '.join(sbatch_test_cmd))
+
+        try:
+            p = subprocess.Popen(sbatch_test_cmd, stderr=subprocess.PIPE)
+            stderr = p.stderr.read()
+            p.wait()
+            assert p.returncode == 0, stderr
+        except:
+            log.exception('Error running sbatch test')
+            raise JobMappingException('An error occurred while trying to schedule this job. Please retry it and if it continues to fail, report it to an administrator using the bug icon.')
+
+        # There is a race condition here, of course. But I don't have a better solution.
+        node = stderr.split()[-1]
+        for i, prefix in enumerate(JETSTREAM_DESTINATION_MAPS[test_destination_id]['cluster_prefixes']):
+            if node.startswith(prefix):
+                cluster = JETSTREAM_DESTINATION_MAPS[test_destination_id]['clusters'][i]
+                destination_prefix = JETSTREAM_DESTINATION_MAPS[test_destination_id]['destination_prefixes'][i]
+                break
+        else:
+            log.error("Could not determine the cluster of node '%s', clusters are: '%s'", node, clusters)
+            raise JobMappingException( 'An error occurred while trying to schedule this job. Please retry it and if it continues to fail, report it to an administrator using the bug icon.' )
+
+        destination_id = '%s_%s' % (destination_prefix, JETSTREAM_DESTINATION_MAPS[default_destination_id]['partition'])
+        destination = app.job_config.get_destination(destination_id)
+
+    if destination_id is None:
+        destination_id = default_destination_id
 
     # Set a walltime if local is the destination and this is a dynamic walltime tool
     if destination_id == LOCAL_DESTINATION and tool_id in RUNTIMES:
