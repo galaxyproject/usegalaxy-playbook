2560b602ee268406955a492606c1a2be9cb52203
Nate Coraor <nate@bx.psu.edu>
2020-09-30 13:56:04 -0500

    Temporarily short circuit the NVC dynamic rule

--- env/common/files/galaxy/dynamic_rules/nvc_dynamic_memory.py
+++ env/common/files/galaxy/dynamic_rules/nvc_dynamic_memory.py
@@ -7,9 +7,30 @@ import logging
 
 from galaxy.util import string_as_bool
 from galaxy.jobs.mapper import JobMappingException
-from pyBamTools.util import guess_array_memory_usage
-from pyBamParser.bam import Reader
+#from pyBamTools.util import guess_array_memory_usage
+#from pyBamParser.bam import Reader
 
+#galaxy.util.submodules ERROR 2020-09-30 13:42:30,316 usegalaxy.jobs.rules.nvc_dynamic_memory dynamic module could not be loaded (traceback follows):
+#Traceback (most recent call last):
+#  File "/cvmfs/main.galaxyproject.org/galaxy/lib/galaxy/util/submodules.py", line 44, in __import_submodules_impl
+#    submodule = importlib.import_module(full_name)
+#  File "/cvmfs/main.galaxyproject.org/deps/_conda/envs/_galaxy_/lib/python3.6/importlib/__init__.py", line 126, in import_module
+#    return _bootstrap._gcd_import(name[level:], package, level)
+#  File "<frozen importlib._bootstrap>", line 994, in _gcd_import
+#  File "<frozen importlib._bootstrap>", line 971, in _find_and_load
+#  File "<frozen importlib._bootstrap>", line 955, in _find_and_load_unlocked
+#  File "<frozen importlib._bootstrap>", line 665, in _load_unlocked
+#  File "<frozen importlib._bootstrap_external>", line 678, in exec_module
+#  File "<frozen importlib._bootstrap>", line 219, in _call_with_frames_removed
+#  File "/srv/galaxy/main/dynamic_rules/usegalaxy/jobs/rules/nvc_dynamic_memory.py", line 11, in <module>
+#    from pyBamParser.bam import Reader
+#  File "/cvmfs/main.galaxyproject.org/venv/lib/python3.6/site-packages/pyBamParser/bam/__init__.py", line 4, in <module>
+#    from ..bai import Reader as BAIReader
+#  File "/cvmfs/main.galaxyproject.org/venv/lib/python3.6/site-packages/pyBamParser/bai/__init__.py", line 4, in <module>
+#    from ..util.odict import odict
+#  File "/cvmfs/main.galaxyproject.org/venv/lib/python3.6/site-packages/pyBamParser/util/odict.py", line 6, in <module>
+#    from UserDict import UserDict
+#ModuleNotFoundError: No module named 'UserDict'
 
 log = logging.getLogger(__name__)
 
@@ -30,6 +51,12 @@ DEFAULT_OVERHEAD = 1024 #MB = 1GB fudge factor to cover non-nucleotide storage
 
 
 def dynamic_nvc_dynamic_memory(app, tool, job):
+    # FIXME hack hack hack
+    destination = 'slurm_normal_16gb'
+    log.debug("(%s) nvc_dynamic_memory dynamic plugin returning '%s' destination", job.id, destination)
+    return destination
+
+    """
     inp_data = dict([(da.name, da.dataset) for da in job.input_datasets])
     inp_data.update([(da.name, da.dataset) for da in job.input_library_datasets])
     params = job.get_param_values(app, ignore_errors=True)
@@ -64,3 +91,4 @@ def dynamic_nvc_dynamic_memory(app, tool, job):
     log.debug("(%s) nvc_dynamic_memory dynamic plugin returning '%s' destination", job.id, DESTINATION)
     log.debug("     nativeSpecification is: %s", destination.params['nativeSpecification'])
     return destination
+"""
