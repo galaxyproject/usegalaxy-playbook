95bc227cf1e7f107aaa7712e21fd56030fefbb5e
Nate Coraor <nate@bx.psu.edu>
2023-02-14 12:48:09 -0600

    TPV config for running hifiasm on HPC

--- env/test/files/galaxy/config/tpv/rank_hpc.py
+++ env/test/files/galaxy/config/tpv/rank_hpc.py
@@ -2,6 +2,12 @@ import random
 from sqlalchemy import func
 from galaxy.jobs.mapper import JobNotReadyException
 
+# this is the same as the regular rank function with the following differences:
+#
+# 1. queued counts are considered before the tag score
+# 2. we make queued counts less precise to increase the importance of the prefer tags in the score
+# 3. prefer destinations with less memory and fewer cores when other factors are equal
+
 if len(candidate_destinations) > 1:
     job_counts = app.model.context.query(app.model.Job.table.c.destination_id, func.count(app.model.Job.table.c.destination_id)).filter(
         app.model.Job.table.c.destination_id.in_(d.id for d in candidate_destinations),
@@ -9,9 +15,9 @@ if len(candidate_destinations) > 1:
     ).group_by(app.model.Job.destination_id).all()
     queued_counts_by_destination = dict((d, c) for d, c in job_counts)
     log.debug(f"#### {queued_counts_by_destination=}")
+    candidate_destinations.sort(key=lambda d: (int(queued_counts_by_destination.get(d.id, 0) / 3), -1 * d.score(entity), d.max_accepted_mem, d.max_accepted_cores, random.randint(0,9)))
     for d in candidate_destinations:
-        log.debug(f"#### {d.id} ({-1 * d.score(entity)}, {queued_counts_by_destination.get(d.id, 0)})")
-    candidate_destinations.sort(key=lambda d: (-1 * d.score(entity), queued_counts_by_destination.get(d.id, 0), random.randint(0,9)))
+        log.debug(f"#### {d.id}: ({int(queued_counts_by_destination.get(d.id, 0) / 3)}, {-1 * d.score(entity)}, {d.max_accepted_mem}, {d.max_accepted_cores})")
     #raise JobNotReadyException()
     final_destinations = candidate_destinations
 else:
